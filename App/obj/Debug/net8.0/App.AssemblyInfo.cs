45262623e20e48de19eee13a94e3f37af1191271
LeeSeungHyun <seunghyune9983@gmail.com>
2024-10-17 17:38:16 +0900

    Remove Some...

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+accccd33c5180620d1fb71aeaa4dcfd8d20cea38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e985bc904163f03b3676c26887231fb32cb00b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
